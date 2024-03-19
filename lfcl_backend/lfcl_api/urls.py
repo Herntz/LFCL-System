e5785c24fca170523146ce64652ba67614aa03cb
Herntz <joeherntz20@gmail.com>
2024-03-19 15:35:16 -0400

    Creation Backend et Frontend

---
+++ lfcl_backend/lfcl_api/urls.py
@@ -0,0 +1,7 @@
+# from django.urls import path
+# from . import views
+
+# urlpatterns = [
+#     path('/', views.your_view_function, name='your-url-name'),
+#     # Ajoutez d'autres patterns d'URL si nécessaire
+# ]
